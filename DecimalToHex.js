5d18f2c1acff6d62beb65572acc51ebce5c6e41a
Subham Mitra <subham.mitra21@gmail.com>
2021-02-06 01:26:41 +0530

    initial commit

---
+++ DecimalToHex.js
@@ -0,0 +1,8 @@
+const input=document.querySelector('#input');
+const output=document.querySelector('#output');
+
+const button=document.querySelector('#convert-button');
+
+button.addEventListener('click',()=>{
+  output.value=parseInt(input.value).toString(16);
+});
